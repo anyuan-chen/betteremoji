ca52b30b727783b00e99e99021e7e955c7b788b6
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-08 12:55:00 -0400

    Changes by Richard Wei

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,15 @@
 package com.company;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("");
+        EmoteList el = new EmoteList();
+        while(true){ //for now
+            String emotion = sc.nextLine();
+            System.out.println(el.getEmojiWithEmotion(emotion));
+        }
     }
 }
